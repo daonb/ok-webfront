87d8a9a8f901f33a8811a57774581f8e621b3552
Benny Daon <bennydaon@gmail.com>
2012-10-09 01:15:48 +0200

    first version of agenda details

--- public/js/apps/agenda.js
+++ public/js/apps/agenda.js
@@ -1,55 +1,48 @@
 define([
     'jquery',
-    'uijet_dir/uijet',
-    'eventbox',
+    'tooltip',
     'mustache'
-], function ($, uijet, Ebox) {
+], function ($) {
     var _window = window,
         Mustache = _window.Mustache,
         app;
 
     app = {
-        init    : function (options) {
-            uijet.init({
-                engine              : function () {
-                    return Mustache.to_html(this.template, this.data || this.context, this.options.partials);
-                },
-                methods_context     : this,
-                methods         : {
-                    publish     : Ebox.notify,
-                    subscribe   : Ebox.listen,
-                    unsubscribe : Ebox.unlisten
-                },
-                TEMPLATES_PATH      : '/views/agenda/',
-                TEMPLATES_EXTENSION : 'html',
-                widgets             : [{
-                    type    : 'Pane',
-                    config  : {
-                        element     : '#agenda',
-                        app_events: {
-                            startup : function () {
-                                this.wake();
-                            }
-                        }
-                    }
-                }, {
-                    type    : 'List',
-                    config  : {
-                        element     : '#members_head',
-                        horizontal  : true
-                    }
-                }, {
-                    type    : 'List',
-                    config  : {
-                        element : '#members',
-                        mixins  : ['Scrolled'],
-                        adapters: ['jqScroll'],
-                        style   : {
-                            height : '60%'
-                        }
-                    }
-                }]
-            });
+        get_uri: function () {
+            // TODO: make this a protoype
+            var url = document.URL;
+            // TODO: use re and lose the constant
+            var s = url.indexOf("/agenda");
+            var path = url.substr(s);
+            return url.substr(0,s)+"/api/v2"+path;
+        },
+        show_agenda : function () {
+            $.get(this.get_uri(), function (data) {
+                data.members.sort(function(x,y) { return y.score-x.score});
+                var min=0;
+                var max=0;
+                // calc to location of each member on the bar
+                for (var i in data.members) {
+                  // TODO: API change - use better resolution than %
+                  var score = data.members[i].score;
+                  data.members[i].left = (score+100-1)/2;
+                  data.members[i].id = i;
+                  if (score>max)
+                    max = score
+                  else if (score<min)
+                    min = score;
+                }
+                //TODO: fix the user's absolute url in OK and remove the next lines
+                for (var i in data.editors)
+                  data.editors[i].absolute_url = "http://oknesset.org"+data.editors[i].absolute_url;
+                // draw the members scores
+                $("#agenda").html(Mustache.render($("#agenda-template").html(),
+                                                   data));
+
+                $("#agenda-bar").css('left', (min+99)/2+'%');
+                $("#agenda-bar").css('width', (max-min+1)/2+'%');
+                $(".score").tooltip();
+            })
         }
     };
 
